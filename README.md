b2bee801543ab2b68cb527280fe16631f59cd0a2
Gloria Lim <glorialimartt@gmail.com>
2024-01-19 22:55:23 -0500

    Set up map view with time zones.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # time-zone-finder
 A web map integrated with a tool designed to identify Time Zones globally.
+
+
+## Sources
+- ArcGIS REST Services Directory: <a href="https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Time_Zones/FeatureServer/0">World_Time_Zones feature layer</a>
---
+++ src/app.js
@@ -0,0 +1,80 @@
+// $(() => {
+//   import Map from "esri/Map";
+//   import MapView from "esri/views/MapView";
+//   import FeatureLayer from "esri/layers/FeatureLayer";
+//   import Legend from "esri/widgets/Legend";
+  
+//   const map = new Map({
+//     basemap: "streets"
+//   });
+  
+//   const view = new MapView({
+//     container: "mapDiv",
+//     map: map,
+//     center: [0, 0],
+//     zoom: 4
+//   })
+  
+//   const FeatureLayer = new FeatureLayer({
+    
+//   })
+// });
+
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/widgets/Legend"
+], function(Map, MapView, FeatureLayer) {
+
+  const map = new Map({
+      basemap: "streets"
+  });
+
+
+  const view = new MapView({
+      container: "mapDiv",
+      map: map,
+      center: [0, 30], // longitude, latitude to center map
+      zoom: 2,
+      constraints: {
+        minZoom: 1 // Use this constraint to avoid zooming out too far
+      }
+  });
+
+  view.when(function() {
+    limitMapView(view);
+  });
+
+  function limitMapView(view) {
+    let initialExtent = view.extent;
+    let initialZoom = view.zoom;
+    view.watch('stationary', function(event) {
+      if (!event) {
+        return;
+      }
+      // If the center of the map has moved outside the initial extent,
+      // then move back to the initial map position (i.e. initial zoom and extent
+      let currentCenter = view.extent.center;
+      if (!initialExtent.contains(currentCenter)) {
+        view.goTo({
+          target: initialExtent,
+          zoom: initialZoom
+        });
+      }
+    });
+  }
+
+
+  const featureLayer = new FeatureLayer({
+      url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Time_Zones/FeatureServer/0",
+      outFields: ["*"], // Adjust based on the data you need
+      popupTemplate: {
+          title: "Zone: {ZONE}", // Replace with your field
+          // content: createChart
+      }
+  });
+  map.add(featureLayer);
+
+});
+
